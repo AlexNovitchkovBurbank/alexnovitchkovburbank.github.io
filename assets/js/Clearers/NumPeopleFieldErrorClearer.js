be5326166d411b96deec2f7ffc417839451e7176
AlexNovitchkovBurbank <ryzen71511@outlook.com>
2024-02-07 17:19:47 -0800

    Reset button works

--- assets/js/Clearers/NumPeopleFieldErrorClearer.js
+++ assets/js/Clearers/NumPeopleFieldErrorClearer.js
@@ -1,6 +1,8 @@
-const ClearNumPeopleFieldFormatting = {
+const NumPeopleFieldErrorClearer = {
     Clear() {
         const mainNumPeopleField = document.querySelector("#num-people-input");
         mainNumPeopleField.classList.remove("input-error");
     }
 }
+
+export default NumPeopleFieldErrorClearer;
--- assets/js/Processors/resetProcessor.js
+++ assets/js/Processors/resetProcessor.js
@@ -1,8 +1,16 @@
-import CurrentOtherFamilyMemberRecordsContainerRemover from "../Removers/CurrentOtherFamilyMemberRecordsContainerRemover"
+const resetProcessor = {
+  Process() {
+    const mainNumPeopleField = document.querySelector("#num-people-input");
+    mainNumPeopleField.classList.remove("input-error");
 
-const resetProcesor = {
-    Process() {
-        ClearNumPeopleFieldFormatting.Clear();
-        CurrentOtherFamilyMemberRecordsContainerRemover.Remove();
-    }
-}
+    const formBodyElement = document.querySelector("#form-body");
+    const recordsContainer = document.querySelector(".form-records-container");
+
+    if (recordsContainer !== null)
+      formBodyElement.removeChild(recordsContainer);
+  },
+};
+
+export default resetProcessor;
+
+// There might be a size limitation in jekyll so I put the remove of any formatting and removing any other containers
