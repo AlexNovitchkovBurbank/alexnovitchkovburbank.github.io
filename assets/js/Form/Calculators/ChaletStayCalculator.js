71cdf02a3738bc1aa7124be14cd3dad68779e4c3
ryzen <ryzen@DESKTOP-NNHA2AR>
2024-02-26 19:08:53 -0800

    started chalet calculations

---
+++ assets/js/Form/Calculators/ChaletStayCalculator.js
@@ -0,0 +1,17 @@
+const ChaletStayContainer = {
+    Calculate() {
+        const numNightsInput = document.querySelector("num-beds-input-for-sleep-in-chalet-checkbox");
+        const numPeopleInput = document.querySelector("num-beds-input-for-sleep-in-chalet-checkbox");
+
+        if (numPeopleInput.textContent > 6) {
+            
+        }
+        else if (numPeopleInput.textContent == 6) {
+
+        }
+        else {
+
+        }
+        //* numNightsInput.textContent * 
+    }
+}
