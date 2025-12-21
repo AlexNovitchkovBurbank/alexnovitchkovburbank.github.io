41a77137852933780cb13c4bead3965e3e32cfda
AlexNovitchkovBurbank <ryzen71511@outlook.com>
2025-12-20 23:08:38 -0800

    inital ts/js coding and folder styles

--- tests/Pages/TsJsPage/TsJsPage.test.tsx
+++ tests/Pages/TsJsPage/TsJsPage.test.tsx
@@ -24,5 +24,17 @@ test("tests that all TS/JS examples present", () => {
   expect(container.children.length).toBe(2);
 
   expect(bodyContainer.className).toBe("exampleSection");
-  expect(bodyContainer?.children.length).toBe(5);
+  expect(bodyContainer?.children.length).toBe(3);
+
+  const folderStructure = bodyContainer?.children.item(0) as HTMLElement;
+  const codeStructure = bodyContainer?.children.item(1) as HTMLElement;
+  const testStructure = bodyContainer?.children.item(2) as HTMLElement;
+
+  expect(folderStructure?.className).toBe("folderStructure");
+  expect(codeStructure?.className).toBe("codeStructure");
+  expect(testStructure?.className).toBe("testStructure");
+
+  expect(folderStructure?.children.length).toBe(2);
+  expect(codeStructure?.children.length).toBe(1);
+  expect(testStructure?.children.length).toBe(1);
 });
