a2a9643689a309f118b35df51818d893a1328e3c
AlexNovitchkovBurbank <ryzen71511@outlook.com>
2025-12-29 08:37:12 -0800

    Added main page links

--- src/Pages/CSharpPage/CSharpPage.tsx
+++ src/Pages/CSharpPage/CSharpPage.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import { formatting } from "../../assets/websiteData.json";
 import FormatCard from "../../Components/FormatCard/FormatCard";
 import Navbar from "../../Components/Navbar/Navbar";
 import "./CSharpPage.css";
 
 const CSharpPage = () => {
+  const hash = window.location.hash
+
+  useEffect(() => {
+    if (hash) {
+      const timer = setTimeout(() => {
+        const element = document.querySelector(hash);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 100);
+
+      return () => clearTimeout(timer); // Clear the timeout because otherwise I would have memory leaks
+    }
+  }, [hash]);
+
   return (
     <>
       <Navbar />
